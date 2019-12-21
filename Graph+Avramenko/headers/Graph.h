5cbc5d2af4770f4d02932a84e1379106efae7508
RinLaNir <46792731+RinLaNir@users.noreply.github.com>
2019-12-21 02:20:26 +0200

    Add files via upload

---
+++ Graph+Avramenko/headers/Graph.h
@@ -0,0 +1,55 @@
+#ifndef GraphH
+#define GraphH
+#include <iostream> 
+#include <limits.h> 
+#include <list> 
+#include <stack> 
+#include <vector>
+#define NINF INT_MIN 
+using namespace std;
+
+// Graph is represented using adjacency list. Every  
+// node of adjacency list contains vertex number of  
+// the vertex to which edge connects. It also  
+// contains weight of the edge  
+class AdjListNode {
+	int v;
+	int weight;
+
+public:
+	AdjListNode(int _v, int _w)
+	{
+		v = _v;
+		weight = _w;
+	}
+	int getV() { return v; }
+	int getWeight() { return weight; }
+};
+
+// Class to represent a graph using adjacency list  
+// representation  
+class Graph {
+	int V; // No. of vertices'  
+
+	// Pointer to an array containing adjacency lists  
+	list<AdjListNode>* adj;
+
+	// A function used by longestPath  
+	void DFSUtil(int v, bool visited[]);
+	void DFS(int src, int prev_len, int* max_len, int* max_vertex, vector<bool> &visit);
+	int longestCable(int n);
+
+public:
+	Graph(int V); // Constructor  
+	~Graph(); // Destructor 
+
+	// function to add an edge to graph  
+	void addEdge(int u, int v, int weight);
+	void connectedComponents();
+	int MaxVertex(int src);
+	bool BFS(int src, int dest, int pred[], int dist[]);
+	int ShortestDistance(int s, int dest);
+	void MaxShortestDistance(int src);
+};
+
+#endif
